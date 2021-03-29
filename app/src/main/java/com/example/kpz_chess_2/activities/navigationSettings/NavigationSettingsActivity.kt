9a5c6f4e333eb826cb39859f91423eabf682aac1
Karolina <karolinakowalczyk1999@gmail.com>
2021-03-29 20:30:46 +0200

    small changes in AndroidManifest.xml

--- app/src/main/java/com/example/kpz_chess_2/activities/navigationSettings/NavigationSettingsActivity.kt
+++ app/src/main/java/com/example/kpz_chess_2/activities/navigationSettings/NavigationSettingsActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kpz_chess_2.R
 
-class navigationSettingsActivity : AppCompatActivity() {
+class NavigationSettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation_settings)
