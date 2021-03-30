37281775e056e2b0b1bd474cf80fa5c569c88da2
Karolina <karolinakowalczyk1999@gmail.com>
2021-03-31 00:03:57 +0200

    working on Navigation UI

--- app/src/main/java/com/example/kpz_chess_2/MainActivity.kt
+++ app/src/main/java/com/example/kpz_chess_2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kpz_chess_2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.kpz_chess_2.activities.game.GameActivity
 import com.example.kpz_chess_2.activities.settings.SettingsActivity
 import com.example.kpz_chess_2.activities.userSettings.UserSettingsActivity
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     fun startGameActivity(view: View){
         val intent = Intent(this, GameActivity::class.java)
         startActivity(intent)
+        Log.d("START", "start Work")
     }
 
     fun continiueGameActivity(view: View){
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun exit(view: View){
-        finish();
+        finish()
         exitProcess(0)
     }
 }
