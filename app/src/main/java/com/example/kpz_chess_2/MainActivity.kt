24ac4721c031f6f37426a07e4d025551259a0431
Forlon <m.fabianowski99@gmail.com>
2021-03-30 17:38:34 +0200

    Rivalchess engine added successfully and works properly

--- app/src/main/java/com/example/kpz_chess_2/MainActivity.kt
+++ app/src/main/java/com/example/kpz_chess_2/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.kpz_chess_2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import karballo.Config
+import com.netsensia.rivalchess.engine.search.Search
+import com.netsensia.rivalchess.model.Board
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val config = Config()
+
+        @kotlin.ExperimentalUnsignedTypes
+        val board = Board.fromFen("6k1/6p1/1p2q2p/1p5P/1P3RP1/2PK1B2/1r2N3/8 b - g3 5 56")
+        val searcher = Search(board)
+        searcher.setMillisToThink(5000)
+        searcher.setNodesToSearch(Int.MAX_VALUE)
+        searcher.setSearchDepth(5)
+        searcher.go()
     }
 }
