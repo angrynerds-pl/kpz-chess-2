6679e076c5fca5709ee76e5590e2804bf5d8698b
Nyatan <domino.ndg@o2.pl>
2021-04-18 19:00:43 +0200

    Implememted saveSearchOptions command

--- app/src/main/java/com/example/kpz_chess_2/utils/ChessWrapper.kt
+++ app/src/main/java/com/example/kpz_chess_2/utils/ChessWrapper.kt
@@ -1,25 +1,27 @@
 package com.example.kpz_chess_2.utils
 
-import com.netsensia.rivalchess.engine.search.Search
-import com.netsensia.rivalchess.enums.SearchState
-import com.netsensia.rivalchess.model.Colour
+import android.util.Log
 import com.example.kpz_chess_2.utils.ChessWrapper.Option.SearchOption.*
 import com.netsensia.rivalchess.config.MAX_SEARCH_DEPTH
 import com.netsensia.rivalchess.config.MAX_SEARCH_MILLIS
+import com.netsensia.rivalchess.engine.search.Search
+import com.netsensia.rivalchess.enums.SearchState
+import com.netsensia.rivalchess.model.Colour
+
 
 @ExperimentalUnsignedTypes
 class ChessWrapper {
     lateinit var search: Search
     var searchOptions: MutableMap<Option.SearchOption, Any?> = mutableMapOf(
-        WHITE_TIME to WHITE_TIME.default,
-        BLACK_TIME to BLACK_TIME.default,
-        WHITE_INC to WHITE_INC.default,
-        BLACK_INC to BLACK_INC.default,
-        MOVES_TO_GO to MOVES_TO_GO.default,
-        MAX_DEPTH to MAX_DEPTH.default,
-        MAX_NODES to MAX_NODES.default,
-        MOVE_TIME to MOVE_TIME.default,
-        IS_INFINITE to IS_INFINITE.default
+            WHITE_TIME to WHITE_TIME.default,
+            BLACK_TIME to BLACK_TIME.default,
+            WHITE_INC to WHITE_INC.default,
+            BLACK_INC to BLACK_INC.default,
+            MOVES_TO_GO to MOVES_TO_GO.default,
+            MAX_DEPTH to MAX_DEPTH.default,
+            MAX_NODES to MAX_NODES.default,
+            MOVE_TIME to MOVE_TIME.default,
+            IS_INFINITE to IS_INFINITE.default
     )
 
     enum class Option{
@@ -36,6 +38,7 @@ class ChessWrapper {
     fun newGame(){
         waitForSearchToComplete()
         search.newGame()
+
     }
 
     fun position(){}
@@ -55,22 +58,22 @@ class ChessWrapper {
     }
 
     fun saveSearchOptions() {
-//        if (searchOptions[IS_INFINITE] as Boolean) {
-//            search.setMillisToThink(MAX_SEARCH_MILLIS)
-//            search.setSearchDepth(MAX_SEARCH_DEPTH - 2)
-//            search.setNodesToSearch(Int.MAX_VALUE)
-//        } else if (moveTime !== -1 || maxNodes !== -1 || maxDepth !== -1) {
-//            search.setMillisToThink(if (moveTime !== -1) moveTime else MAX_SEARCH_MILLIS)
-//            search.setSearchDepth(if (maxDepth !== -1) maxDepth else MAX_SEARCH_DEPTH - 2)
-//            search.setNodesToSearch(if (maxNodes > 0) maxNodes else Int.MAX_VALUE)
-//        } else if (whiteTime !== -1) {
-//            val calcTime: Int =
-//                (if (search.mover == Colour.WHITE) whiteTime else blackTime) / if (movesToGo === 0) 120 else movesToGo
-//            val guaranteedTime: Int = if (search.mover == Colour.WHITE) whiteInc else blackInc
-//            val timeToThink: Int =
-//                calcTime + guaranteedTime - Uci.UCI_TIMER_SAFTEY_MARGIN_MILLIS.getValue()
-//            search.setMillisToThink(timeToThink)
-//        }
+        if (searchOptions[IS_INFINITE] as Boolean) {
+            search.setMillisToThink(MAX_SEARCH_MILLIS)
+            search.setSearchDepth(MAX_SEARCH_DEPTH - 2)
+            search.setNodesToSearch(Int.MAX_VALUE)
+        } else if ((searchOptions[MOVE_TIME] as Int) != -1 || (searchOptions[MAX_NODES] as Int) != -1 || (searchOptions[MAX_DEPTH] as Int) != -1) {
+            search.setMillisToThink(if ((searchOptions[MOVE_TIME] as Int) != -1) searchOptions[MOVE_TIME] as Int else MAX_SEARCH_MILLIS)
+            search.setSearchDepth(if ((searchOptions[MAX_DEPTH] as Int) != -1) searchOptions[MAX_DEPTH] as Int else MAX_SEARCH_DEPTH - 2)
+            search.setNodesToSearch(if ((searchOptions[MAX_NODES] as Int) > 0) searchOptions[MAX_NODES] as Int else Int.MAX_VALUE)
+        } else if (searchOptions[WHITE_TIME] as Int != -1) {
+            val calcTime: Int =
+                (if (search.mover == Colour.WHITE) (searchOptions[WHITE_TIME] as Int) else (searchOptions[BLACK_TIME] as Int)) /
+                        (if (searchOptions[MOVES_TO_GO] as Int == 0) (120) else (searchOptions[MOVES_TO_GO] as Int))
+            val guaranteedTime: Int = if (search.mover == Colour.WHITE) (searchOptions[WHITE_INC] as Int) else (searchOptions[BLACK_INC] as Int)
+            val timeToThink: Int = calcTime + guaranteedTime
+            search.setMillisToThink(timeToThink)
+        }
     }
 
     fun stop(){
